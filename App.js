50c9c4a35a237d8e493873d96b5b45189285d356
Ayush Sinha <ayushsinhajsr20@gmail.com>
2023-03-29 19:12:14 +0530

    Complete

--- App.js
+++ App.js
@@ -1,12 +1,51 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, Button} from 'react-native';
+import React, { useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const App = () => {
+
+  const [name , setName] = useState(null)
+  const [age, setAge] = useState(null)
+
+  const user_details = {Name : "Ayush",Age: 21}
+
+  const setData = () => {
+    AsyncStorage.setItem("user_details",JSON.stringify(user_details))
+    console.log("set data")
+  }
+
+  const showData = async() => {
+    let user = await AsyncStorage.getItem("user_details")
+    user = JSON.parse(user)
+
+    setName(user.Name)
+    setAge(user.Age)
+  }
+
+  const clearData = () => {
+    AsyncStorage.clear()
+  }
+
+
   return (
     <View>
-      <Text>App</Text>
-    </View>
-  )
-}
+      <View style={{ margin: 20,}}>
+        <Button title="Set Data" onPress={setData} />
+      </View>
 
-export default App
+      <View style={{ margin: 20,}}>
+        <Button title="Show Data" onPress={showData} />
+      </View>
+
+      <View style={{ margin: 20,}}>
+        <Button title="Clear Data" onPress={clearData} />
+      </View>
+
+      {
+        name ? <Text style={{fontSize: 19, color:"#000", margin: 20}}>{name}  {age}</Text> : null
+      }
+    </View>
+  );
+};
+
+export default App;
